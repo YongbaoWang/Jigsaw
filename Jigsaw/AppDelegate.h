8626c3db01843eeb91ec49f95adfb9279c792b99
YongbaoWang <wangyongbao2009@hotmail.com>
2015-01-31 17:24:57 +0800

    Initial Commit

---
+++ Jigsaw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jigsaw
+//
+//  Created by Ever on 15/1/31.
+//  Copyright (c) 2015年 Ever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
